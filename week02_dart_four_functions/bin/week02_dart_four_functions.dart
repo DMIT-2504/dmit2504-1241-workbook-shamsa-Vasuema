2197a439f445c1533944305fe73f29dabb69b279
Vasuema <133081276+Vasuema@users.noreply.github.com>
2024-09-13 03:21:49 -0600

    udate four functions

---
+++ week02_dart_four_functions/bin/week02_dart_four_functions.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:week02_dart_four_functions/week02_dart_four_functions.dart'
+    as week02_dart_four_functions;
+
+void main(List<String> arguments) {
+//   Write a program to tally up total change from user’s pockets. The method GetTotal() will have
+// number of pennies, nickels, dimes, quarters, loonies and twoonies passed into it and will return the
+// total amount. All values entered must be integers and positive. Validation is required to ensure a
+// negative value is not allowed
+
+  print("Enter the number of pennies");
+  int pennies = validateInput();
+
+  print("Enter the number of nickels");
+  int nickels = validateInput();
+
+  print("Enter the number of dimes");
+  int dimes = validateInput();
+
+  print("Enter the number of quarters");
+  int quarters = validateInput();
+
+  print("Enter the number of loonies");
+  int loonies = validateInput();
+
+  print("Enter the number of toonies");
+  int toonies = validateInput();
+
+  double total = getTotal(pennies, nickels, dimes, quarters, loonies, toonies);
+  print('total value is: $total');
+}
+
+int validateInput() {
+  int? value;
+  while (true) {
+    String? input = stdin.readLineSync();
+    value = int.tryParse(input!);
+    if (value != null && value > 0) {
+      return value;
+    } else {
+      print('Please provide a valid positive number');
+    }
+  }
+}
+
+double getTotal(int pennies, int nickels, int dimes, int quarters, int loonies,
+    int toonies) {
+  const double penny = 0.01;
+  const double nickel = 0.05;
+  const double dime = 0.10;
+  const double quarter = 0.25;
+  const double loonie = 1.0;
+  const double toonie = 2.0;
+
+  double totalValue = (pennies * penny) +
+      (nickels * nickel) +
+      (dimes * dime) +
+      (quarters * quarter) +
+      (loonies * loonie) +
+      (toonies * toonie);
+
+  return totalValue;
+}
---
+++ week02_dart_four_functions/test/week02_dart_four_functions_test.dart
@@ -0,0 +1,8 @@
+import 'package:week02_dart_four_functions/week02_dart_four_functions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
