017223e22844fd7a45934bd9adf1a049c06b8d49
Vasuema <133081276+Vasuema@users.noreply.github.com>
2024-09-23 00:39:54 -0600

    async exercise

---
+++ week03_async_exercise/dart_async_exercise/bin/dart_async_exercise.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_async_exercise/dart_async_exercise.dart'
+    as dart_async_exercise;
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${dart_async_exercise.calculate()}!');
+  stdout.writeln('Please enter a word:');
+  var userInput = stdin.readLineSync();
+  //print(userInput);
+  getWordMeaning(userInput);
+}
+
+///async function that returns decoded json data from an api
+Future<dynamic> getJson(api) async {
+  var url = Uri.parse(api);
+  var response = await http.get(url);
+  var decodedJson = jsonDecode(response.body);
+  return decodedJson;
+}
+
+///async function that gets the json from an api endpoint
+void getWordMeaning(userInput) async {
+  try {
+    var apiData = await getJson(
+        'https://api.dictionaryapi.dev/api/v2/entries/en/$userInput');
+
+    if (apiData == null) {
+      print('Please enter a word that exists');
+    }
+    var listItems = apiData[0]['meanings'];
+    //above and below variable may be combined as needed
+    var desiredElement = listItems[0]['definitions'][0]['definition'];
+    print(desiredElement);
+  } catch (e) {
+    print('Error occurred: $e');
+  }
+}
---
+++ week03_async_exercise/dart_async_exercise/test/dart_async_exercise_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_async_exercise/dart_async_exercise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
