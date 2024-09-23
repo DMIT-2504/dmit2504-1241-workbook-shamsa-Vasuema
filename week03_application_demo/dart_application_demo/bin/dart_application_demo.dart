a301f9e17fd1403290caff9016067aceed248128
Vasuema <133081276+Vasuema@users.noreply.github.com>
2024-09-22 21:49:15 -0600

    add demos

---
+++ week03_application_demo/dart_application_demo/bin/dart_application_demo.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_demo/dart_application_demo.dart' as dart_application_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_demo.calculate()}!');
+}
