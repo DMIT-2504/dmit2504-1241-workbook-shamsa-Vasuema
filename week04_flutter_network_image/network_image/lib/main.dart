c3097a02f14e071f8b228e84e2dfb52d5a4d9408
Vasuema <133081276+Vasuema@users.noreply.github.com>
2024-10-01 03:40:39 -0600

    dog api

--- week04_flutter_network_image/network_image/lib/main.dart
+++ week04_flutter_network_image/network_image/lib/main.dart
@@ -13,41 +13,41 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Network Image',
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello suckers!',
-            style: TextStyle(fontSize: 36),
-          ),
-        ),
-      ),
+      home: ImageScreen()
     );
   }
 }
 
 //stateful widget to track state of network image
-class ImageScreen extends StatefulWidget{
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({super.key});
+
+//this instantiates the state object
+  @override
+  _ImageState createState(){
+    return _ImageState();
+  } 
 
 }
 
 //state class
-class ImageState extends State<ImageScreen>{
+class _ImageState extends State<ImageScreen> {
   String? _imageURL;
 
   //call the api endpoint
   //callback function to get the image
-  Future<void> _fetchImage() async{
-    final response = await http.get(Uri.parse('https://dog.ceo/api/breeds/image/random'));
-    if(response.statusCode == 200){
+  Future<void> _fetchImage() async {
+    final response =
+        await http.get(Uri.parse('https://dog.ceo/api/breeds/image/random'));
+    if (response.statusCode == 200) {
       final data = json.decode(response.body);
+
+      //rebuild UI once image is received
       setState(() {
         _imageURL = data['message'];
       });
     }
   }
-
-  
-  
 
   @override
   Widget build(BuildContext context) {
@@ -60,21 +60,26 @@ class ImageState extends State<ImageScreen>{
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              height: 500,
-              width: double.infinity, //this will take up the entire width of the screen; cannot be applied to the height
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black)
-              ),
-              child: _imageURL == null ? Center(child: Text('Please try again'))
-              : Image.network(
-                _imageURL!, // needs null assertion (cannot be null)
-              fit: BoxFit.contain //contain prevents cropping
-              ,)
-            ),
-            SizedBox(
+                height: 500,
+                width: double
+                    .infinity, //this will take up the entire width of the screen; cannot be applied to the height
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: _imageURL == null
+                    ? const Center(child: Text('Please try again'))
+                    : Image.network(
+                        _imageURL!, // needs null assertion (cannot be null)
+                        fit: BoxFit.contain //contain prevents cropping
+                        ,
+                      )),
+            //space between image and button
+            const SizedBox(
+              //this sizebox is the gap
               height: 20,
             ),
-            ElevatedButton(onPressed: _fetchImage, child: Text('Load Image'))
+            //button to load the image and trigger UI build
+            ElevatedButton(
+                onPressed: _fetchImage, child: const Text('Load Image'))
           ],
         ),
       ),
