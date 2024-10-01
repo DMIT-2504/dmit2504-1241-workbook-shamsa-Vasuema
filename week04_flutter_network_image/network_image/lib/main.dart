73a42fb50c1b21b6bdf27d4b8a7af8ba8732ac9f
Vasuema <133081276+Vasuema@users.noreply.github.com>
2024-10-01 03:29:23 -0600

    ...

--- week04_flutter_network_image/network_image/lib/main.dart
+++ week04_flutter_network_image/network_image/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
@@ -10,12 +12,70 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Network Image',
       home: Scaffold(
         body: Center(
           child: Text(
             'Hello suckers!',
             style: TextStyle(fontSize: 36),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+//stateful widget to track state of network image
+class ImageScreen extends StatefulWidget{
+
+}
+
+//state class
+class ImageState extends State<ImageScreen>{
+  String? _imageURL;
+
+  //call the api endpoint
+  //callback function to get the image
+  Future<void> _fetchImage() async{
+    final response = await http.get(Uri.parse('https://dog.ceo/api/breeds/image/random'));
+    if(response.statusCode == 200){
+      final data = json.decode(response.body);
+      setState(() {
+        _imageURL = data['message'];
+      });
+    }
+  }
+
+  
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Network Image'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 500,
+              width: double.infinity, //this will take up the entire width of the screen; cannot be applied to the height
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black)
+              ),
+              child: _imageURL == null ? Center(child: Text('Please try again'))
+              : Image.network(
+                _imageURL!, // needs null assertion (cannot be null)
+              fit: BoxFit.contain //contain prevents cropping
+              ,)
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: _fetchImage, child: Text('Load Image'))
+          ],
         ),
       ),
     );
